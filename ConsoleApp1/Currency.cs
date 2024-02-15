7cd4e5413c4f2fb83b9ec36941a493e49a76c3d3
VitaliiPytymko <vitaliipytymko@gmail.com>
2024-02-15 19:49:59 +0200

    1

--- ConsoleApp1/Currency.cs
+++ ConsoleApp1/Currency.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
-//"r030":840,"txt":"Долар США","rate":37.6234,"cc":"USD","exchangedate":"12.02.2024"
+
 namespace ConsoleApp1
 {
     public class Currency
